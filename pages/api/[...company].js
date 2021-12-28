15a76b99d888932a83ded1969f77f9af644b6a96
axelhagman <axel.hagman@redeye.se>
2021-12-28 15:52:33 +0100

    init

---
+++ pages/api/[...company].js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+import Cors from "cors";
+
+// Initializing the cors middleware
+const cors = Cors();
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+const companyScreenshot = (req, res) => {
+  (async () => {
+    await runMiddleware(req, res, cors);
+    const {
+      query: { company },
+    } = req;
+    const companyId = company[0].split(".")[0];
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+    const page = await browser.newPage();
+    page.setViewport({ width: 870, height: 210 });
+    await page.goto(
+      `https://www.redeye.se/utility/qualityRatingImage?companyId=${companyId}`,
+      {
+        waitUntil: "networkidle2",
+      }
+    );
+    const file = await page.screenshot({ omitBackground: true });
+
+    await browser.close();
+    res.statusCode = 200;
+    res.send(file);
+  })();
+};
+
+export default companyScreenshot;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Redeye Image generation app</h1>
+      <a href='https://www.redeye.se/' style={{ color: '#FFA699' }}>
+        www.redeye.se
+      </a>
+    </div>
+  );
+}
