27928e0320fc9f9d630d792312efddad2e79c426
axelhagman <axel.hagman@redeye.se>
2022-10-13 13:39:19 +0200

    updated cors

--- pages/api/[...company].js
+++ pages/api/[...company].js
@@ -1,5 +1,5 @@
-const puppeteer = require("puppeteer");
-import Cors from "cors";
+const puppeteer = require('puppeteer');
+import Cors from 'cors';
 
 // Initializing the cors middleware
 const cors = Cors();
@@ -18,20 +18,30 @@ function runMiddleware(req, res, fn) {
   });
 }
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (origin === 'http://localhost:3000/') {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 const companyScreenshot = (req, res) => {
   (async () => {
-    await runMiddleware(req, res, cors);
+    await runMiddleware(req, res, cors(corsOptions));
     const {
       query: { company },
     } = req;
-    const companyId = company[0].split(".")[0];
-    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+    const companyId = company[0].split('.')[0];
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     page.setViewport({ width: 870, height: 210 });
     await page.goto(
       `https://www.redeye.se/utility/qualityRatingImage?companyId=${companyId}`,
       {
-        waitUntil: "networkidle2",
+        waitUntil: 'networkidle2',
       }
     );
     const file = await page.screenshot({ omitBackground: true });
